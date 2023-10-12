4347e4aa13d3693c7077f059d61ea29e62cbed60
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-10-12 09:32:38 +0000

    Deploying to gh-pages from @ YuriyAtamanenko/Phonebook@3c2abebf0aa055382d936394bf7e9568ed161a5a 🚀

--- static/js/75.45156af9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[75],{75:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(329);function c(){return(0,t.jsxs)("section",{style:{display:"flex",flexDirection:"column",alignItems:"center",paddingTop:50},children:[(0,t.jsx)("p",{children:"Welcome to a great app"}),(0,t.jsx)("h1",{style:{fontSize:48},children:"Phonebook"})]})}}}]);
-//# sourceMappingURL=75.45156af9.chunk.js.map
