242427029cfd038c88e612bfa63bf8b3074e1512
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-11-28 11:24:55 +0200

    Del dotenv

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,11 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-require('dotenv').config();
-
-const { SERVER_HOST } = process.env;
-
-axios.defaults.baseURL = SERVER_HOST;
+axios.defaults.baseURL = 'https://phonebook-api-3gs7.onrender.com';
 // axios.defaults.baseURL = 'http://localhost:3000';
 
 const token = {
